9162f7a8b7cf5872b82fc2217c44bdf014e85af2
Mati <matiasfornes@gmail.com>
2019-01-30 01:06:30 +0100

    feat: dockerize car-list-service & deploy to heroku app

--- apiserver/server.js
+++ apiserver/server.js
@@ -1,27 +1,27 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
 class HttpError extends Error {
-  constructor(code, message = 'Uh oh, something went wrong.') {
+  constructor(code, message = "Uh oh, something went wrong.") {
     super(`${message} HTTP code ${code}`);
     this.code = code;
   }
 }
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-const makes = require('./makes.json');
-const models = require('./models.json');
-const vehicles = require('./vehicles.json');
+const makes = require("./makes.json");
+const models = require("./models.json");
+const vehicles = require("./vehicles.json");
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
 
   // all cors okay.
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     res.writeHead(200);
     res.end();
     return;
@@ -34,14 +34,14 @@ async function handleRequest(req, res) {
   const urlParts = url.parse(req.url);
   try {
     switch (urlParts.pathname) {
-      case '/api/makes':
+      case "/api/makes":
         handleGet(req, res, makes);
         break;
-      case '/api/models':
-        handleGet(req, res, models, ['make']);
+      case "/api/models":
+        handleGet(req, res, models, ["make"]);
         break;
-      case '/api/vehicles':
-        handleGet(req, res, vehicles, ['make', 'model']);
+      case "/api/vehicles":
+        handleGet(req, res, vehicles, ["make", "model"]);
         break;
       default:
         throw new HttpError(404);
@@ -56,7 +56,7 @@ async function handleRequest(req, res) {
 }
 
 function handleGet(req, res, data, filters) {
-  if (req.method !== 'GET') {
+  if (req.method !== "GET") {
     throw new HttpError(405);
   }
 
@@ -69,8 +69,8 @@ function handleGet(req, res, data, filters) {
   if (filters) {
     filteredData = filterData(req, data, filters);
 
-    if(data === models) {
-      filteredData = filteredData.map(({make, model}) => model);
+    if (data === models) {
+      filteredData = filteredData.map(({ make, model }) => model);
     }
   }
 
@@ -83,17 +83,20 @@ function filterData(req, data, properties) {
   const urlParts = url.parse(req.url, true);
 
   if (!properties.every(property => urlParts.query[property])) {
-    throw new HttpError(422, `You must specify the following parameters: ${properties.join(', ')}.`);
+    throw new HttpError(
+      422,
+      `You must specify the following parameters: ${properties.join(", ")}.`
+    );
   }
 
   return data.filter(item =>
     properties.every(property => {
-      const regx = new RegExp(urlParts.query[property], 'i');
+      const regx = new RegExp(urlParts.query[property], "i");
       return item[property].search(regx) >= 0;
     })
   );
 }
 
-server.listen(port, function () {
+server.listen(port, function() {
   console.log("Server listening on: http://localhost:%s", port);
 });
