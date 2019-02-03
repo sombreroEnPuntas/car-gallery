db6f8375df72e93f17eadcbe51efd856a787676d
Mati <matiasfornes@gmail.com>
2019-02-03 23:41:57 +0100

    fix: better error handling on car-service data fetch

--- data/mocks.js
+++ data/mocks.js
@@ -4,19 +4,8 @@ export const makesList = ['ford', 'opel']
 export const modelsList = ['explorer', 'fiesta']
 
 // API
-export const errorAPIResponse = {
-  error: true,
-  message:
-    'invalid json response body at https://car-list-service.herokuapp.com/api/some-end-point reason: Unexpected token U in JSON at position 0',
-  type: 'invalid-json',
+export const internalError = {
+  error: `Wut?. SyntaxError`,
 }
 
-export const badInputAPIResponse = {
-  error: true,
-  message: `Invalid input provided as parameter.`,
-  type: 'bad-input',
-}
-
-export const makesAPIResponse = makesList
-
-export const modelsAPIResponse = modelsList
+export const errorAPIResponse = { error: `418 I'm a teapot` }
